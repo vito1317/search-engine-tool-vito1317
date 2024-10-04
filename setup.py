45590963dd147cb5391a66408bd464efa71aa746
vito <vito95311@gmail.com>
2024-10-04 16:31:22 +0800

    你的提交訊息

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="search-engine-tool-vito",
+    name="search-engine-tool-vito1317",
     version="0.3.0",
     packages=find_packages(),
     install_requires=[
